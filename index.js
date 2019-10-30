07aff30a629ae6e386fba5b4d4fdaa1dea993e77
kly001 <karenly1961@gmail.com>
2019-10-30 12:11:05 -0400

    GET users

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ const port = 4321;
 server.listen(port, () => {
     console.log(`**Server listening on port ${port}***`);
 })
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.send("Hello from index.js inside Node-API1-Project");
+})
+
+server.get('/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ success: false, err });
+        });
+});
