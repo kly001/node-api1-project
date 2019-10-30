b2044a96b41ee3166ec912bfb9b428efe9785318
kly001 <karenly1961@gmail.com>
2019-10-30 11:58:58 -0400

    Installed Express; setup server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// implement your API here
+const db = require("./data/db.js");
+const express = require("express");
+
+const server = express();
+
+const port = 4321;
+
+server.listen(port, () => {
+    console.log(`**Server listening on port ${port}***`);
+})
