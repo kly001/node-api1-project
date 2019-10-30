a3b83658327ad859ca2ae6fc1197b86e20f7049d
kly001 <karenly1961@gmail.com>
2019-10-30 17:59:17 -0400

    PUT api/users/:id

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.get("/", (req, res) => {
     res.send("Hello from index.js inside Node-API1-Project");
 })
 
-//GET request to "api/users"
+//Return an array of all users: GET request to "api/users"
 server.get("/api/users", (req, res) => {
     db.find()
         .then(users => {
@@ -28,7 +28,7 @@ server.get("/api/users", (req, res) => {
         });
 });
 
-//POST request to "api/users"
+//Create a user: POST request to "api/users"
 server.post("/api/users", (req,res)=> {
     const userInfo = req.body;
     if(!userInfo.name || !userInfo.bio) {
@@ -44,7 +44,7 @@ server.post("/api/users", (req,res)=> {
     }
 })
 
-//DELETE request to "api/users/:id"
+//Remove user with specified Id: DELETE request to "api/users/:id"
 server.delete("/api/users/:id", (req,res) => {
     const {id} = req.params
     db.remove(id)
@@ -60,3 +60,24 @@ server.delete("/api/users/:id", (req,res) => {
     })
 })
 
+//Update a users with specified Id; returns new information:  PUT request to "api/users/id"
+server.put("/api/users/:id", (req,res) => {
+    const {id} = req.params;
+    const userInfo = req.body;
+   
+    if(!userInfo.name || userInfo.bio) {
+        res.status(400).json({errorMessage:"Please provide name and bio for the user"})
+    } else {
+        db.update(id, userInfo)
+        .then(user => {
+            if(user) {
+                res.status(200).json({success: true, user})
+            } else {
+                res.status(404).json({message:"The user with the specified Id does not exist"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error:"The user information couold not be modified"})
+        })
+    }
+})
